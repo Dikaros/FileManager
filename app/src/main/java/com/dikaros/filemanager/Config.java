f465427a042f7bcb31b2bf1e41147bfec90860a8
Dikaros <a229732074@gmail.com>
2016-05-17 22:12:14 +0800

    简单文件管理器 没有实现删除文件夹,复制粘贴重命名

---
+++ app/src/main/java/com/dikaros/filemanager/Config.java
@@ -0,0 +1,54 @@
+package com.dikaros.filemanager;
+
+/**
+ * Created by mac on 16/4/16.
+ */
+public class Config {
+    /**
+     *  视频格式
+     */
+    public static final String TYPE_VIDEO = "mp4,3gp,rm,avi,mov,wmv,rmvb,flv";
+    /**
+     * 音乐格式
+     */
+    public static final String TYPE_MUSIC = "mp3,wav,wma,ogg,ape,acc,flac";
+
+    /**
+     * 代码格式
+     */
+    public static final String TYPE_CODE = "java,c,m,cpp,cs,html,css,js,swift,asp,aspx,php,jsp,xml,h";
+
+    /**
+     * 图片格式
+     */
+    public static final String TYPE_IMAGE = "png,jpg,jpeg,gif,bmp,svg";
+    /**
+     * word格式
+     */
+    public static final String TYPE_WORD = "doc,docx";
+    /**
+     * ppt格式
+     */
+    public static final String TYPE_POWER_POINT = "ppt,pptx";
+    /**
+     * excel格式
+     */
+    public static final String TYPE_EXCEL = "xls,xlsx";
+    /**
+     * pdf格式
+     */
+    public static final String TYPE_PDF = "pdf";
+    /**
+     * 文本格式
+     */
+    public static final String TYPE_TEXT = "txt";
+    /**
+     * 压缩格式
+     */
+    public static final String TYPE_ZIP = "zip,rar,7z";
+    /**
+     * apk
+     */
+    public static final String TYPE_APK = "apk";
+
+}
